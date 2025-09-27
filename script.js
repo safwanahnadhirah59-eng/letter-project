ce5e54535fac336a76eea3827995682f265cd281
safwanahnadhirah59-eng <safwanahnadhirah59@gmail.com>
2025-09-27 15:30:52 +0800

    Update script.js
    script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I know we've been through ups and downs,but my feelings for you have never faded.\n\ I still love you deeply,and the thought of losing you completely hurts me.\n\n You means so much to me and i don't want to let go of what we had i want to fight for us because you're worth it.\n\nI always love you my mizi ,Sayang and my big baby\n\nPls forgive me sayng i'm not tryna to force you but i can't help myself and i will do anything for us .`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,3 +21,4 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
